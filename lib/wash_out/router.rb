852238a40cca39226643c176ed6927a5b8addc09
artem <a.melnikov@carex.su>
2018-09-07 18:43:20 +0600

    Test1

--- lib/wash_out/router.rb
+++ lib/wash_out/router.rb
@@ -1,3 +1,5 @@
+require 'nori'
+
 module WashOut
   # This class is a Rack middleware used to route SOAP requests to a proper
   # action of a given SOAP controller.
@@ -9,7 +11,18 @@ module WashOut
     def call(env)
       controller = @controller_name.constantize
 
-      if soap_action = parse_soap_action(env)
+      soap_action = controller.soap_config.soap_action_routing ? env['HTTP_SOAPACTION'].to_s.gsub(/^"(.*)"$/, '\1')
+                        : ''
+      if soap_action.blank?
+        parsed_soap_body = nori(controller.soap_config.snakecase_input).parse(soap_body env)
+        return nil if parsed_soap_body.blank?
+
+        soap_action = parsed_soap_body.values_at(:envelope, :Envelope).try(:compact).try(:first)
+        soap_action = soap_action.values_at(:body, :Body).try(:compact).try(:first) if soap_action
+        soap_action = soap_action.keys.first.to_s if soap_action
+      end
+
+      unless soap_action.blank?
         # RUBY18 1.8 does not have force_encoding.
         soap_action.force_encoding('UTF-8') if soap_action.respond_to? :force_encoding
 
